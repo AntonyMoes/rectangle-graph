4b0c0371830862140f8f461735418f3d44b161a9
AntonyMo <32247865+AntonyMoes@users.noreply.github.com>
2021-08-22 21:27:34 +0300

    Implement first versions of Rectangle and Connection

---
+++ Assets/Scripts/Connection.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Connection : MonoBehaviour {
+    Rectangle _target1;
+    Rectangle _target2;
+    
+    public void Setup(Rectangle target1, Rectangle target2) {
+        _target1 = target1;
+        _target2 = target2;
+    }
+    
+    public void UpdatePosition() {
+        var pos1 = _target1.transform.position;
+        var pos2 = _target2.transform.position;
+        var distanceVector = pos2 - pos1;
+        
+        transform.position = (pos1 + pos2) / 2;
+        transform.right = distanceVector.normalized;
+        
+        var scale = transform.localScale;
+        scale.x = distanceVector.magnitude;
+        transform.localScale = scale;
+    }
+}
---
+++ Assets/Scripts/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Rectangle : MonoBehaviour {
+    public readonly HashSet<Connection> Connections = new HashSet<Connection>();
+
+    Vector3 _lastPosition;
+
+    void Update() {
+        var newPosition = transform.position;
+        if (newPosition != _lastPosition) {
+            foreach (var connection in Connections) {
+                connection.UpdatePosition();
+            }
+        }
+
+        _lastPosition = newPosition;
+    }
+}
